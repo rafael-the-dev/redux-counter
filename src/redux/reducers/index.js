64ff8dcda68d2c211f253f62737474dfe844d383
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-15 19:10:30 +0200

    defining redux actions, reducer, initial state, and store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,16 @@
-export const reducer = () => {};
+import { initialState } from '../state'
+import { decrement, increment} from '../actions'
+
+export const reducer = ( state=initialState, action) => {
+    switch(action.type) {
+        case increment().type: {
+            return { ...state, value: state.value + 1 };
+        }
+        case decrement().type: {
+            return { ...state, value: state.value - 1 };
+        }
+        default: {
+            return state;
+        }
+    }
+};
--- src/redux/state/index.js
+++ src/redux/state/index.js
@@ -1,3 +1,3 @@
 export const initialState = {
-    counter: 0
+    value: 0
 }
