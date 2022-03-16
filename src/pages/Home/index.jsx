68c1e383ce52fb7f9387a433055bf6c25ba31687
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-16 12:58:04 +0200

    adding redux Provider component, and adding event handlers

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,29 @@
 import './styles.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment} from '../../redux/actions'
+import { useCallback } from 'react'
 
 const Home = () => {
+    const counter = useSelector(state => state.value);
+    const dispatch = useDispatch();
+    const clickHandler = useCallback(action => () => dispatch(action), [ dispatch ]);
+
     return (
         <main>
             <section className="flex flex-column justify-center align-items-center container">
-                <h1 className="container__counter">0</h1>
+                <h1 className="container__counter">{ counter }</h1>
                 <div className="container__buttons-container">
-                    <button className="container__button container__button--increment">Increment</button>
-                    <button className="container__button">Decrement</button>
+                    <button 
+                        className="container__button container__button--increment"
+                        onClick={clickHandler(increment())}
+                        >
+                        Increment
+                    </button>
+                    <button 
+                        className="container__button"
+                        onClick={clickHandler(decrement())}>
+                        Decrement
+                    </button>
                 </div>
             </section>
         </main>
